fcacb36dba3ac90f2bda22f1f8ddce96023b6872
Gaurav Tiwari <tiwarigaurav1998@gmail.com>
2017-12-03 10:28:03 +0530

    DOM using JQuery

--- Using local storage/todolist.js
+++ Using local storage/todolist.js
@@ -1,77 +1,71 @@
-window.onload=()=>{
-let addbtn=document.getElementById('add-btn');
-let value=document.getElementById('add-new-todo');
-let list=document.getElementById('list');
-let delBtn = document.getElementById('del-btn');
-let warning=document.getElementById('warning');
-let message=document.getElementById('message');
+$(()=>{
+let addbtn= $("#add-btn");
+let value= $("#add-new-todo");
+let list=$("#list");
+let delBtn = $("#del-btn");
+let warning=$("#warning");
+let message=$("#message");
 let count=-1;
-list.innerHTML="";
-addbtn.onclick = ()=> {
-    let taskvalue = value.value;
+list.html("");
+addbtn.click(()=> {
+   let taskvalue = value.val();
+   alert(taskvalue);
     if(taskvalue!=="")
     {
-        warning.style.display='none';
+        warning.css("display","none");
         if(count<1)
         {
-            message.style.display='none';
+            message.css("display","none");
                 count=0;
         }
         count++;
-        value.value = "";
-        let task = document.createElement('li');
-        task.className = 'list-group-item';
-        let taskname=document.createElement('span');
-        taskname.innerHTML = taskvalue;
-        task.appendChild(taskname);
+        value.val("");
+        let task = $("<li></li>");
+         task.addClass("list-group-item");
+        let taskname=$("<span></span>");
+        taskname.html(taskvalue);
+        task.append(taskname);
 
-        let removeElement=document.createElement('i');
-        removeElement.className='fa fa-times col-1 border ml-3';
-        removeElement.title='Remove Task';
-        task.appendChild(removeElement);
+            let removeElement=$("<i></i>")
+            removeElement.addClass("fa fa-times col-1 border ml-3").attr("title","Remove Task");
+            task.append(removeElement);
 
+            let increasePriority=$("<i></i>");
+            increasePriority.addClass("fa fa-arrow-up col-1 border").attr("title","Increase Priority");
+            task.append(increasePriority);
 
-        let increasePriority=document.createElement('i');
-        increasePriority.className='fa fa-arrow-up col-1 border';
-        increasePriority.title='Increase Priority';
-        task.appendChild(increasePriority);
+        let decreasePriority=$("<i></i>");
+        decreasePriority.addClass("fa fa-arrow-down col-1 border").attr("title","Decrease Priority");
+        task.append(decreasePriority);
 
-        let decreasePriority=document.createElement('i');
-        decreasePriority.className='fa fa-arrow-down col-1 border';
-        decreasePriority.title='Decrease Priority';
-        task.appendChild(decreasePriority);
+        list.append(task);
 
-        list.appendChild(task);
-        removeElement.onclick=function () {
-            list.removeChild(removeElement.parentNode);
-            count--;
-            console.log(count);
-            if(count===0)
-            {
-                message.innerHTML="Great! You've completed all tasks.";
-                message.style.display='block';
-            }
+        removeElement.click(()=>{
+            removeElement.parent().remove();
+        });
 
-        };
-        increasePriority.onclick=function () {
-            let p=increasePriority.parentNode;
-            let a=p.previousSibling.childNodes[0].innerHTML;
-            p.previousSibling.childNodes[0].innerHTML=p.childNodes[0].innerHTML;
-            p.childNodes[0].innerHTML=a;
-        };
-        decreasePriority.onclick=function () {
-            let p=decreasePriority.parentNode;
-            let a=p.nextSibling.childNodes[0].innerHTML;
-            p.nextSibling.childNodes[0].innerHTML=p.childNodes[0].innerHTML;
-            p.childNodes[0].innerHTML=a;
-        }
-    }
-    else{
-        warning.innerHTML="Enter the task correctly!!!";
-        warning.style.display='block';
-    }
+//         increasePriority.onclick=function () {
+//             let p=increasePriority.parentNode;
+//             let a=p.previousSibling.childNodes[0].innerHTML;
+//             p.previousSibling.childNodes[0].innerHTML=p.childNodes[0].innerHTML;
+//             p.childNodes[0].innerHTML=a;
+//         };
 
-};
+//         decreasePriority.onclick=function () {
+//             let p=decreasePriority.parentNode;
+//             let a=p.nextSibling.childNodes[0].innerHTML;
+//             p.nextSibling.childNodes[0].innerHTML=p.childNodes[0].innerHTML;
+//             p.childNodes[0].innerHTML=a;
+//         }
+//     }
+//     else{
+//         warning.innerHTML="Enter the task correctly!!!";
+//         warning.style.display='block';
+//     }
+//
+
+    }}
+);
 
 delBtn.onclick= ()=>
 {
@@ -86,4 +80,4 @@ delBtn.onclick= ()=>
     }
 };
 
-};
+});
